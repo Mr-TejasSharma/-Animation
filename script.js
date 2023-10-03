67775cd1338afa888f2c3db7282914fc852249d2
Mr-TejasSharma <tejas.sharma111002@gmail.com>
2023-10-03 14:49:53 +0530

    Added Landing Page

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+var open=document.querySelector(".open")
+var game=document.querySelector(".game")
 var day=document.querySelector(".switchD")
 var night=document.querySelector(".switchN")
 var sun=document.querySelector(".sun")
@@ -10,11 +12,12 @@ var wheel2=document.querySelector(".wheel2")
 var body=document.querySelector("body")
 var light=document.querySelector(".light")
 var engine=new Audio("./Assets/Audios/engine.mp3")
-var throttle=new Audio("./Assets/Audios/throttle.mp3")
+var throttle=new Audio("./Assets/Audios/throttle.wav")
 var brake=new Audio("./Assets/Audios/brake.mp3")
 var drift=new Audio("./Assets/Audios/drift.mp3")
 var horn=new Audio("./Assets/Audios/horn.wav")
 var start=new Audio("./Assets/Audios/start.mp3")
+var music=new Audio("./Assets/Audios/music.mp3")
 
 
 day.addEventListener("click",function(){
@@ -47,6 +50,8 @@ night.addEventListener("click",function(){
 
 body.addEventListener("keydown", function(event) {
   if (event.keyCode== "83") {
+    game.style.display="block";
+    open.style.display="none";
     frame.style.left="5"+"vw";
     light.style.left="32.5"+"vw";
     wheel1.style.left="7.2"+"vw";
@@ -59,11 +64,11 @@ body.addEventListener("keydown", function(event) {
 
 
 body.addEventListener("keydown", function(event) {
-  if (event.keyCode== "37"&&(parseInt((frame.style.left).split('v')[0])-5)>-5) {
-    frame.style.left=`${parseInt((frame.style.left).split('v')[0])-5}vw`;
-    light.style.left=`${parseInt((light.style.left).split('v')[0])-5}vw`;
-    wheel1.style.left=`${parseInt((wheel1.style.left).split('v')[0])-5}vw`;
-    wheel2.style.left=`${parseInt((wheel2.style.left).split('v')[0])-5}vw`;
+  if (event.keyCode== "37"&&(parseInt((frame.style.left).split('v')[0])-20)>-20) {
+    frame.style.left=`${parseInt((frame.style.left).split('v')[0])-20}vw`;
+    light.style.left=`${parseInt((light.style.left).split('v')[0])-20}vw`;
+    wheel1.style.left=`${parseInt((wheel1.style.left).split('v')[0])-20}vw`;
+    wheel2.style.left=`${parseInt((wheel2.style.left).split('v')[0])-20}vw`;
     brake.play();
   }
 });
@@ -71,21 +76,23 @@ body.addEventListener("keydown", function(event) {
 
 body.addEventListener("keydown", function(event) {
   if (event.keyCode == "38") {
+    if(parseInt((frame.style.top).split('v')[0])>29){
+      drift.play();
+    }
     frame.style.top="29.5"+"vw";
     light.style.top="28"+"vw";
     wheel1.style.top="36"+"vw";
     wheel2.style.top="36"+"vw";
-    drift.play();
   }
 });
 
 
 body.addEventListener("keydown", function(event) {
-  if (event.keyCode== "39"&&(parseInt((frame.style.left).split('v')[0])+5)<75) {
-    frame.style.left=`${parseInt((frame.style.left).split('v')[0])+5}vw`;
-    light.style.left=`${parseInt((light.style.left).split('v')[0])+5}vw`;
-    wheel1.style.left=`${parseInt((wheel1.style.left).split('v')[0])+5}vw`;
-    wheel2.style.left=`${parseInt((wheel2.style.left).split('v')[0])+5}vw`;
+  if (event.keyCode== "39"&&(parseInt((frame.style.left).split('v')[0])+10)<81) {
+    frame.style.left=`${parseInt((frame.style.left).split('v')[0])+10}vw`;
+    light.style.left=`${parseInt((light.style.left).split('v')[0])+10}vw`;
+    wheel1.style.left=`${parseInt((wheel1.style.left).split('v')[0])+10}vw`;
+    wheel2.style.left=`${parseInt((wheel2.style.left).split('v')[0])+10}vw`;
     throttle.play();
   }
 });
@@ -93,17 +100,19 @@ body.addEventListener("keydown", function(event) {
 
 body.addEventListener("keydown", function(event) {
   if (event.keyCode== "40") {
+    if(parseInt((frame.style.top).split('v')[0])<37){
+      drift.play();
+    }
     frame.style.top="37"+"vw";
     light.style.top="35.5"+"vw";
     wheel1.style.top="43.5"+"vw";
     wheel2.style.top="43.5"+"vw";
-    drift.play();
   }
 });
 
 
-body.addEventListener("keydown", function(event) {
-  if (event.keyCode== "72") {
+body.addEventListener("keypress", function(event) {
+  if (event.key== "h") {
     horn.play();
   }
 });
@@ -113,12 +122,13 @@ body.addEventListener("keydown", function(event) {
   if (event.keyCode== "77") {
     engine.pause();
     start.pause();
+    music.pause();
   }
 });
 
 
 body.addEventListener("keydown", function(event) {
   if (event.keyCode== "80") {
-    engine.play();
+    music.play();
   }
 });
